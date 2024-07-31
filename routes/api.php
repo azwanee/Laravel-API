24446f029d53be7c653762da28a5f72c8efe7f4e
Ghazwan <ghazwanfauzan811@gmail.com>
2024-07-31 08:46:11 +0700

    AUTH

--- routes/api.php
+++ routes/api.php
@@ -1,29 +1,30 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\LigaController;
-use App\Http\Controllers\Api\KlubController;
-use App\Http\Controllers\Api\PemainController;
-use App\Http\Controllers\Api\FanController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
- */
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::get('/user', function (Request $request) {
     return $request->user();
+})->middleware('auth:sanctum');
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::post('logout', [AuthController::class, 'logout']);
+    // controller lainnya yang kemarin sudah dibuat simpan dibawah
+    Route::resource('liga', LigaController::class)->except(['edit', 'create']);
+    Route::resource('klub', KlubController::class)->except(['edit', 'create']);
+    Route::resource('pemain', PemainController::class)->except(['edit', 'create']);
+    Route::resource('fan', FanController::class)->except(['edit', 'create']);
+    // teruskan
 });
+
+// auth route
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
 
 // Route::get('liga', [LigaController::class, 'index']);
 // Route::post('liga', [LigaController::class, 'store']);
 // Route::get('liga/{id}', [LigaController::class, 'show']);
 // Route::put('liga/{id}', [LigaController::class, 'update']);
 // Route::delete('liga/{id}', [LigaController::class, 'destroy']);
-
-Route::resource('liga', LigaController::class)->except(['edit', 'create']);
-Route::resource('klub', KlubController::class)->except(['edit', 'create']);
-Route::resource('pemain', PemainController::class)->except(['edit', 'create']);
-Route::resource('fan', FanController::class)->except(['edit', 'create']);
